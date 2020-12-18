51027949c7ed3bdeacc3923c3745524a40c8490e
Ahel Das Chatterjee <aheldc@gmail.com>
2020-12-18 15:27:06 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (54.07%) - LeetHub

---
+++ missing-number/missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length,sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        
+        return (int)n*(n+1)/2-sum;
+    }
+}
