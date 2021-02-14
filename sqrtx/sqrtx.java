0d93e4a7243e09a4d1aa5bb230cc6eada5f9ad17
Ahel Das Chatterjee <aheldc@gmail.com>
2021-02-14 19:17:35 +0530

    Time: 1 ms (99.98%), Space: 35.7 MB (98.13%) - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0)return 0;
+        int start=1,end=x; 
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid<=x/mid)start=mid+1;
+            else end=mid-1;
+        }
+        
+        return end;
+    }
+}
