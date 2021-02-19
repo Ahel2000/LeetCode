feda763e7e7476a0dc9f814f75420708807b8154
Ahel Das Chatterjee <aheldc@gmail.com>
2021-02-19 16:38:14 +0530

    Time: 2 ms (14.47%), Space: 38.9 MB (58.18%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int max_so_far=0,max_ending_here=0;
+        
+        for(int i=0;i<n;i++){
+            max_ending_here+=nums[i];
+            if(max_ending_here>max_so_far)max_so_far=max_ending_here;
+            else if(max_ending_here<0)max_ending_here=0;
+        }
+        
+        Arrays.sort(nums);
+        if(nums[n-1]<0)return nums[n-1];
+        else return max_so_far;
+    }
+}
