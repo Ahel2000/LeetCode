7c7c973f70d1fe996a7f138c545f654fa8f60e10
Ahel Das Chatterjee <aheldc@gmail.com>
2021-04-14 11:41:58 +0530

    Time: 1 ms (53.76%), Space: 39.3 MB (40.30%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,38 +1,40 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    HashMap<Integer,List<Integer>> map=new HashMap<>();
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> l=new ArrayList<>();
-        if(root==null)return Collections.emptyList();
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            List<Integer> temp=new ArrayList<>();
-            Queue<TreeNode> t=new LinkedList<>();
-            while(!queue.isEmpty()){
-                TreeNode tmp=queue.poll();
-                temp.add(tmp.val);
-                if(tmp.left!=null)t.add(tmp.left);
-                if(tmp.right!=null)t.add(tmp.right);
-            }
-            l.add(temp);
-            queue=t;
-        }
-        
-        return l;
-    }
+    HashMap<Integer,List<Integer>> map=new HashMap<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> l=new ArrayList<>();
+        if(root==null)return Collections.emptyList();
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            List<Integer> temp=new ArrayList<>();
+            Queue<TreeNode> q=new LinkedList<>();
+            
+            while(!queue.isEmpty()){
+                if(queue.peek().left!=null)q.add(queue.peek().left);
+                if(queue.peek().right!=null)q.add(queue.peek().right);
+                temp.add(queue.poll().val);
+            }
+            
+            l.add(temp);
+            queue=q;
+        }
+        
+        return l;
+    }
 }
