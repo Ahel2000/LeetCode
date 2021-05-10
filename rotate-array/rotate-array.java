60978a6aa22ee854a21c17ffadcd613218cbaf7d
Ahel Das Chatterjee <aheldc@gmail.com>
2021-05-10 18:13:57 +0530

    Time: 1 ms (49.45%), Space: 56.1 MB (20.29%) - LeetHub

---
+++ rotate-array/rotate-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        int i=0,j=nums.length-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        
+        i=k;j=nums.length-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        
+        i=0;j=k-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
