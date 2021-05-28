2bf006b891be38d1872cf373740b3478c9577cce
Ahel Das Chatterjee <aheldc@gmail.com>
2021-05-28 17:57:01 +0530

    Time: 0 ms (100.00%), Space: 38.3 MB (90.86%) - LeetHub

---
+++ battleships-in-a-board/battleships-in-a-board.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        int count = 0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] == 'X'){
+                    count++;
+                    dfs(board,i,j);
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    public void dfs(char[][] board, int i, int j){
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length)return;
+        if(board[i][j] == '.')return;
+        
+        board[i][j] = '.';
+        dfs(board,i-1,j);
+        dfs(board,i+1,j);
+        dfs(board,i,j-1);
+        dfs(board,i,j+1);
+    }
+}
