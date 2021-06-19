32e27f1b8b0d75a3035bb4cf4f8dd52fe0c810e3
Ahel Das Chatterjee <aheldc@gmail.com>
2021-06-19 16:50:51 +0530

    Time: 2 ms (66.56%), Space: 39.6 MB (27.06%) - LeetHub

---
+++ number-of-closed-islands/number-of-closed-islands.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int closedIsland(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if((i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1) 
+                  && grid[i][j] == 0){
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 0){
+                    dfs(grid,i,j);count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    public void dfs(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length)return;
+        if(grid[i][j] == 1)return;
+        
+        grid[i][j] = 1;
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+}
