d2f59ae8a397b231a810c801e76a8b76661603f2
Ahel Das Chatterjee <aheldc@gmail.com>
2021-07-15 09:07:47 +0530

    Time: 1 ms (99.95%), Space: 39.4 MB (55.76%) - LeetHub

--- path-sum-ii/path-sum-ii.java
+++ path-sum-ii/path-sum-ii.java
@@ -1,33 +1,39 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> paths=new ArrayList<>();
-        findPaths(root,sum,new ArrayList<Integer>(),paths);
-        return paths;
-    }
-    
-    public void findPaths(TreeNode root,int sum,List<Integer> current,List<List<Integer>> paths){
-        if(root==null)return;
-        current.add(root.val);
-        if(root.val==sum && root.left==null && root.right==null){
-            paths.add(current);
-            return;
-        }
-        findPaths(root.left,sum-root.val,new ArrayList<Integer>(current),paths);
-        findPaths(root.right,sum-root.val,new ArrayList<Integer>(current),paths);
-    }
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> paths=new ArrayList<>();
+        findPaths(root,sum,new ArrayList<Integer>(),paths);
+        return paths;
+    }
+    
+    public void findPaths(TreeNode root,int sum,List<Integer> current,List<List<Integer>> paths){
+        if(root == null)return;
+        if(root.left ==null && root.right == null){
+            if(sum == root.val){
+                current.add(root.val);
+                paths.add(new ArrayList<>(current));
+                current.remove(current.size() - 1);
+            }
+            return;
+        }
+        
+        current.add(root.val);
+        findPaths(root.left,sum - root.val,current,paths);
+        findPaths(root.right,sum - root.val,current,paths);
+        current.remove(current.size() - 1);
+    }
 }
