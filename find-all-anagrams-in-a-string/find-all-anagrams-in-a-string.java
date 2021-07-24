b54974da89c17f417972a30687db1a9c5e55e12f
Ahel Das Chatterjee <aheldc@gmail.com>
2021-07-24 22:32:49 +0530

    Time: 6 ms (91.09%), Space: 39.7 MB (87.65%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,19 +1,38 @@
 class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-        if(n<m)return Collections.emptyList();
-        List<Integer> l=new ArrayList<>();
-        char ch[]=p.toCharArray();
-        Arrays.sort(ch);
-        String z=new String(ch);
-        for(int i=0;i<=n-m;i++){
-            String tmp=s.substring(i,i+m);
-            char []q=tmp.toCharArray();
-            Arrays.sort(q);
-            String r=new String(q);
-            if(r.equals(z))l.add(i);
-        }
-        return l;
-    }
+    public List<Integer> findAnagrams(String s, String p) {
+        int n=s.length();
+        int m=p.length();
+        if(n<m)return Collections.emptyList();
+        
+        int hash[] = new int[26];
+        for(int i=0;i<m;i++)hash[p.charAt(i) - 'a']++;
+        
+        List<Integer> l = new ArrayList<>();
+        int i = 0,j = 0;
+        int temp[] = new int[26];
+        while(i < n && j < n){
+            temp[s.charAt(j) - 'a']++;
+            if(j - i + 1 < m){
+                j++;
+            }else{
+                if(isAnagram(temp,hash)){
+                    l.add(i);
+                }
+                
+                temp[s.charAt(i) - 'a']--;
+                i++;
+                j++;
+            }
+        }
+        
+        return l;
+    }
+    
+    public boolean isAnagram(int[] temp,int[] hash){
+        for(int i=0;i<26;i++){
+            if(temp[i] != hash[i])return false;
+        }
+        
+        return true;
+    }
 }
