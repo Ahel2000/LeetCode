0f680fcfc0f06866c39f12c3f319d1c925993c00
Ahel Das Chatterjee <aheldc@gmail.com>
2021-08-10 16:26:10 +0530

    Time: 0 ms (100.00%), Space: 35.8 MB (48.78%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.java
+++ unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,13 +1,22 @@
 class Solution {
-    public int numTrees(int n) {
+    
+    public int numTrees(int n){
         if(n<0)return 0;
         if(n==1||n==0)return 1;
         if(n==2)return 2;
-        int res=0;
-        for(int i=1;i<=n;i++){
-            res+=numTrees(i-1)*numTrees(n-i);
+        
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3;i<=n;i++){
+            for(int j=0;j<=i - 1;j++){
+                dp[i] += dp[i - j - 1] * dp[j];
+            }
         }
-        return res;
+        
+        return dp[n];
     }
 
 }
