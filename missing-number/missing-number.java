578450c2050f0f932962f36aae15056304b51d13
Ahel Das Chatterjee <aheldc@gmail.com>
2021-09-07 10:30:13 +0530

    Time: 0 ms (100.00%), Space: 39 MB (89.58%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n=nums.length,sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        
-        return (int)n*(n+1)/2-sum;
-    }
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int exp = n * (n + 1) / 2;
+        int act = 0;
+        for(int i=0;i<n;i++)act += nums[i];
+        
+        return exp - act;
+    }
 }
