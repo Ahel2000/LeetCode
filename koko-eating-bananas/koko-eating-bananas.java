9c0935c3fae6490a7e6b942884fe6c10d5d1e970
Ahel Das Chatterjee <aheldc@gmail.com>
2021-09-10 07:24:29 +0530

    Time: 37 ms (15.41%), Space: 40 MB (80.80%) - LeetHub

--- koko-eating-bananas/koko-eating-bananas.java
+++ koko-eating-bananas/koko-eating-bananas.java
@@ -1,21 +1,23 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int start=1,end=1000000000;
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if(isValid(piles,mid,h))end=mid;
-            else start=mid+1;
+        int start = 1,end = (int)1e9;
+        
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(isValid(piles,h,mid))end = mid;
+            else start = mid + 1;
         }
         
         return start;
     }
     
-    public boolean isValid(int[] piles,int x,int h){
-        int total=0;
-        for(int i=0;i<piles.length;i++){
-            total+=(int)Math.ceil(piles[i]*1.0/x);
+    public boolean isValid(int[] piles,int h,int x){
+        int total = 0;
+        for(int num : piles){
+            total += (int)Math.ceil(num * 1.0 / x);
+            if(total > h)return false;
         }
         
-        return total<=h;
+        return true;
     }
 }
