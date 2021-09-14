4eb16921a36288c6b167a64dafd5c1cab77fa227
Ahel Das Chatterjee <aheldc@gmail.com>
2021-09-14 12:39:07 +0530

    Time: 90 ms (9.04%), Space: 72.2 MB (5.01%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -5,6 +5,7 @@ class Solution {
         
         for(int i=0;i<=amount;i++){
             for(int j=0;j<=coins.length;j++){
+                
                 if(i == 0){
                     dp[i][j] = 0;
                     continue;
@@ -13,7 +14,7 @@ class Solution {
                 if(j == 0)continue;
                 
                 if(coins[j-1] <= i){
-                    dp[i][j] = Math.min(1 + dp[i - coins[j-1]][j],dp[i][j-1]);
+                    dp[i][j] = Math.min(dp[i][j-1],1 + dp[i-coins[j-1]][j]);
                 }else{
                     dp[i][j] = dp[i][j-1];
                 }
